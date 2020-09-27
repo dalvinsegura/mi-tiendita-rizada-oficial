2b90b83d030ffdd9929bd2dd9f55eeab85a390de
Dalvin <dalvinsegura18@hotmail.com>
2020-09-27 16:52:15 -0400

    Se añadio una funcion que cuando le den click en la imagen de algun producto lo dirija hacia el y tambien se añadio en el pie de los productos la barra de navegacion entre las paginas de productos (todos los cambio surgieron solo en la pagina tienda.html)

---
+++ js/tienda.js
@@ -0,0 +1,111 @@
+// (function(){
+let products_1 = document.getElementById(`products-1`);
+let products_main = document.getElementById(`products-main`);
+let view_product = document.getElementById(`view_product`);
+let img_link_1 = document.getElementById(`img_link_1`);
+let img_link_2 = document.getElementById(`img_link_2`);
+let img_link_3 = document.getElementById(`img_link_3`);
+let img_link_4 = document.getElementById(`img_link_4`);
+let img_link_5 = document.getElementById(`img_link_5`);
+let img_link_6 = document.getElementById(`img_link_6`);
+let img_link_7 = document.getElementById(`img_link_7`);
+let img_link_8 = document.getElementById(`img_link_8`);
+let img_link_9 = document.getElementById(`img_link_9`);
+let img_link_10 = document.getElementById(`img_link_10`);
+let img_link_11 = document.getElementById(`img_link_11`);
+let img_link_12 = document.getElementById(`img_link_12`);
+
+
+
+
+function redi_1(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_2(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_3(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_4(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_5(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_6(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_7(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_8(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_9(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_10(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_11(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+function redi_12(){
+    
+    window.location="https://www.afroliterd.com/producto/cepillo-para-rizos/";
+
+} 
+
+
+
+img_link_1.addEventListener(`click`, redi_1)
+img_link_2.addEventListener(`click`,redi_2)
+img_link_3.addEventListener(`click`,redi_3)
+img_link_4.addEventListener(`click`,redi_4)
+img_link_5.addEventListener(`click`,redi_5)
+img_link_6.addEventListener(`click`,redi_6)
+img_link_7.addEventListener(`click`,redi_7)
+img_link_8.addEventListener(`click`,redi_8)
+img_link_9.addEventListener(`click`,redi_9)
+img_link_10.addEventListener(`click`,redi_10)
+img_link_11.addEventListener(`click`,redi_11)
+img_link_12.addEventListener(`click`,redi_12)
+
+
+
+
+// })
